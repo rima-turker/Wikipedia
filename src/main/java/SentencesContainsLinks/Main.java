a11b719d13f05799d5b146450d5d1bd869039777
rwashahi <rimaturker@gmail.com>
2017-09-17 17:05:13 +0200

    finished running

--- src/main/java/SentencesContainsLinks/Main.java
+++ src/main/java/SentencesContainsLinks/Main.java
@@ -1,17 +1,15 @@
 package SentencesContainsLinks;
 
-public class Main {
-	private static String WIKI_FILES_FOLDER="/home/rtue/workspace/Wikipedia/src/main/resources/wikiFiles";
-	private static int NUMBER_OF_THREADS = 1;
+import org.apache.log4j.Logger;
 
+public class Main {
+	private static final Logger LOG  = Logger.getLogger(SentencesWithLink.class);
 	public static void main(String[] args) 
 	{
-		//SentencesWithLink sentences = new SentencesWithLink(args[0],args[1]);
-		System.out.println("a[0]= "+args[0]);
+		SentencesWithLink sentences = new SentencesWithLink(Integer.parseInt(args[0]),args[1]);
+		System.out.println("a[0]= "+Integer.parseInt(args[0]));
 		System.out.println("a[1]= "+args[1]);
-		SentencesWithLink sentences = new SentencesWithLink(NUMBER_OF_THREADS,WIKI_FILES_FOLDER);
 		sentences.checkWikiPages();
-		
 	}
 
 }
