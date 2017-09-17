3589241e4d7db099bddb3aede6283f525f4846be
rwashahi <rimaturker@gmail.com>
2017-09-17 17:34:58 +0200

    play

--- src/main/java/SentencesContainsLinks/Main.java
+++ src/main/java/SentencesContainsLinks/Main.java
@@ -1,9 +1,8 @@
 package SentencesContainsLinks;
 
-import org.apache.log4j.Logger;
 
 public class Main {
-	private static final Logger LOG  = Logger.getLogger(SentencesWithLink.class);
+	
 	public static void main(String[] args) 
 	{
 		SentencesWithLink sentences = new SentencesWithLink(Integer.parseInt(args[0]),args[1]);
